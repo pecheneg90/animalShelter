dd5c5addfe9ea17b82592ec4fd1c1a7b795cd968
pecheneg90 <manjavha@gmail.com>
2022-11-13 21:11:33 +0100

    Initial commit

---
+++ src/test/java/sky/pro/animalshelter/AnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.animalshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
